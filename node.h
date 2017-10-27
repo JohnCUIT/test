3dcd141e894d92001484d5eb094a9d1dc87bab95
JohnCUIT <632913662@qq.com>
2017-10-27 21:03:45 +0800

    提交链表程序

---
+++ node.h
@@ -0,0 +1,26 @@
+/*===============================================================
+*   Copyright (C) 2017 All rights reserved.
+*   
+*   File Name:	node.h
+*   Creater:		Shengjiang He
+*   Create Date:	2017-10-27
+*   Detail:		
+*
+*   Update Log:	
+*
+================================================================*/
+#ifndef __NODE_H_
+#define __NODE_H_
+
+typedef	struct
+{
+	void* pNext;
+	void* pPrev;
+	void* pData;
+	void* (*GetData)(void* this);
+	void* (*GetNextNode)(void* this);
+	void* (*GetPrevNode)(void* this);
+}Node;
+
+#endif //__NODE_H_
+
